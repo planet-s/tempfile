40334bf8eedd41606de03dd52fc5078d025e8c04
Steven Allen <steven@stebalien.com>
2015-05-17 12:59:30 -0400

    Update docs for bf41e73fb42fb7eec3b78e751334b64912e21125
    Reorganize.

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['tempfile'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='tempfile/struct.PersistError.html' title='tempfile::PersistError'>PersistError</a>&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/std/io/error/struct.Error.html' title='std::io::error::Error'>Error</a>",];
+implementors['tempfile'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;PersistError&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/std/io/error/struct.Error.html' title='std::io::error::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
