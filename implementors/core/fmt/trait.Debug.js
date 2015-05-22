5e561868da5587329fbea09549ed234cc05a81d4
Steven Allen <steven@stebalien.com>
2015-05-21 20:40:38 -0400

    Update docs for 637c8e9ecb207e033414941e8e3bed2841912511
    Implement debug for unnamed tempfiles.
    
    Use handle/file descriptor to identify.

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['tempfile'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='tempfile/struct.NamedTempFile.html' title='tempfile::NamedTempFile'>NamedTempFile</a>",];
+implementors['tempfile'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='tempfile/struct.NamedTempFile.html' title='tempfile::NamedTempFile'>NamedTempFile</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='tempfile/struct.TempFile.html' title='tempfile::TempFile'>TempFile</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
