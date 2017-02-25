1b2c3a5480176f9a920fff11c3f891c777abe15b
Steven Allen <steven@stebalien.com>
2017-02-25 13:10:27 -0800

    Update docs for 0b7e0d573a2351d4f0a6e4ed833bc6ffdbc0dd4f
    Document/test minimum rust version.
    
    And set it to 1.9.0

--- implementors/std/sys/imp/ext/io/trait.AsRawFd.js
+++ implementors/std/sys/imp/ext/io/trait.AsRawFd.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
-implementors["tempfile"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.AsRawFd.html' title='std::sys::imp::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='tempfile/struct.NamedTempFile.html' title='tempfile::NamedTempFile'>NamedTempFile</a>",];
+implementors["tempfile"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.AsRawFd.html' title='trait std::sys::imp::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='tempfile/struct.NamedTempFile.html' title='struct tempfile::NamedTempFile'>NamedTempFile</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
