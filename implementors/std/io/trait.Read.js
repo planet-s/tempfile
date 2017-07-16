f951f520de2255402aa72d7a87e6f04dd2eae4f6
Steven Allen <steven@stebalien.com>
2017-07-16 00:14:32 -0700

    Update docs for 02061c79a3534a9eebff55e33baabb681fe66493
    Release 2.1.6
    
    * Remove build script and bump minimum rustc version.

--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
-implementors["tempfile"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='trait std::io::Read'>Read</a> for <a class='struct' href='tempfile/struct.NamedTempFile.html' title='struct tempfile::NamedTempFile'>NamedTempFile</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='trait std::io::Read'>Read</a> for &amp;'a <a class='struct' href='tempfile/struct.NamedTempFile.html' title='struct tempfile::NamedTempFile'>NamedTempFile</a>",];
+implementors["tempfile"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"tempfile/struct.NamedTempFile.html\" title=\"struct tempfile::NamedTempFile\">NamedTempFile</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for &amp;'a <a class=\"struct\" href=\"tempfile/struct.NamedTempFile.html\" title=\"struct tempfile::NamedTempFile\">NamedTempFile</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
